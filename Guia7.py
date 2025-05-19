def35327a11cb94f426eaad35a4d7f0d1b46b2fb
Franco Zaina <119088313+francozaina@users.noreply.github.com>
2025-05-19 16:41:33 -0300

    ejercicios de la Guia 7

---
+++ Guia7.py
@@ -0,0 +1,99 @@
+import math
+from typing import List, Dict, Tuple
+#Guia 7
+#Ejercicio 1
+#1.1
+def pertenece (lista:List[int], num:int) -> bool:
+    for i in lista:
+        if (num == i):
+            res = True
+            print(res)
+            return res       
+    res = False
+    print (res)
+    return res
+pertenece ([1,2,3],4)
+#def pertenece2 (lista:List[int], num:int) -> bool:
+#1.2
+def divide_a_todos (lista:List[int], num:int) -> bool:
+    cont = 0
+    for i in lista:
+        if (i%num==0):
+            cont = cont + 1
+    if (cont == len(lista)):
+        res = True
+        print (res)
+        return res
+    else:
+        res = False
+        print (False)
+        return False 
+divide_a_todos ([4,6,8],3)        
+def divide_a_todosPACHO (lista:List[int], num:int) -> bool:
+    cont = 0
+    for i in lista:
+        if (i%num!=0):
+            return False       
+    return True
+#1.3
+def suma_a_todos (lista:List[int]) -> int:
+    total = 0
+    for i in lista:
+        total = total + i
+    print (total)
+suma_a_todos ([1,2,3])
+#1.4
+def maximo (lista:List[int]) -> int:
+    max = 0
+    for i in lista:
+        if (max < i):
+            max = i
+    print (max)
+maximo ([1,2,4,3])
+#1.5
+def minimo (lista:List[int]) -> int:
+    min = 1
+    for i in lista:
+        if (min > i):
+            min = i           
+    print (min)
+minimo ([5,2,4,1])
+#1.6
+def ordenados (lista:List[int]) -> bool:
+    cont = 0
+    anterior = 0
+    for i in lista:
+        if (i>anterior):
+            cont = cont + 1
+            anterior = i
+    if (cont == len(lista)):
+        res = True
+        print (res)
+    else:
+        res = False
+        print (res)
+ordenados ([1,2,3,4,5])
+
+
+
+
+
+
+
+    
+
+    
+
+    
+
+
+
+
+
+
+
+
+          
+
+
+    
